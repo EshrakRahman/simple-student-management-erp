2d1f5ae6e88aa931f28b58e42a9f5d71cc313c99
Eshrak Rahman <eshrakurrahman@gmail.com>
2026-04-14 21:30:09 +0600

    dashboard done

---
+++ app/Filament/Widgets/LatestStudents.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Student;
+use Filament\Actions\BulkActionGroup;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+class LatestStudents extends TableWidget
+{
+    protected int|string|array $columnSpan = 'full';
+    protected static ?int $sort = 2;
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(fn(): Builder => Student::query()->latest()->limit(5))
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('class.name')
+                    ->badge(),
+                TextColumn::make('section.name')
+                    ->badge(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                //
+            ])
+            ->recordActions([
+                //
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    //
+                ]),
+            ]);
+    }
+}
